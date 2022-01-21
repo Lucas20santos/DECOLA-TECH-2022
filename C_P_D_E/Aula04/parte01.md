65f960fcbe18731562f205a685c43795137767ff
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2022-01-21 20:28:03 -0300

    subindo arquivo

---
+++ C_P_D_E/Aula04/parte01.md
@@ -0,0 +1,13 @@
+# Modificadores
+
+## Readonly
+
+O modificar readonly (somente leitura) bloqueia um campo
+contra alterações que não sejam em sua inicialização ou pelo
+próprio construtor
+
+## Constante
+
+Uma constante representa um valor que somente pode ser
+atribuido no momento de sua inicialização, e não pode ser
+modificado posteriormente.
--- Projeto2/Program.cs
+++ Projeto2/Program.cs
@@ -7,12 +7,18 @@ namespace Projeto2
 
         static void Main(string[] args)
         {
-            // Console.Clear();
+            Console.Clear();
             // Pessoas p1 = new Pessoas("Lucas", "Santos");
             // p1.Apresentar();
 
-            Log log = new Log();
-             
+            // Log log = new Log();
+
+            Data data = new Data();
+            data.setMes(2);
+            data.ApresentarMes();
+            data.setMes(20);
+            data.ApresentarMes();
+
         }
     }
 }
